1c92396afbaabbc535a7022f043cecc70784bd0f
saceli <301811192+saceli@users.noreply.github.com>
2026-08-03 14:50:44 +0200

    fix: set static IPs to 192.168.178.XX instead of 192.168.1.XX

--- hosts/hardware/laptop/static-ip.nix
+++ hosts/hardware/laptop/static-ip.nix
@@ -26,7 +26,7 @@
 
         ipv4 = {
           method = "manual";
-          addresses = "192.168.1.30/24";
+          addresses = "192.168.178.30/24";
           gateway = "192.168.178.1";
         };
         
