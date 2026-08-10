3f6fa74db54ab934c5bd10867001a0e45daedefa
saceli <301811192+saceli@users.noreply.github.com>
2026-08-10 20:22:27 +0200

    sdfjnrehjejnverj

--- modules/services/searxng/microvm.nix
+++ modules/services/searxng/microvm.nix
@@ -10,7 +10,7 @@ in
 
       imports = [
         ./oci-containers.nix
-        (config.microvm.globalOptions { inherit index mac; })
+        (config.microvm.globalOptions { inherit index mac config; })
       ];
 
       networking.firewall.allowedTCPPorts = [ 8080 ];
