5b8e3283e2aecd2fc1e9df2ebc45d18bac091319
saceli <301811192+saceli@users.noreply.github.com>
2026-08-10 21:47:26 +0200

    3rd error, hopefully fixed

--- modules/sops/raspi.nix
+++ modules/sops/raspi.nix
@@ -5,4 +5,9 @@
     sopsFile = ../../secrets/raspi/secrets.yaml;
     key = "searxng/secret_key";
   };
+
+  systemd.tmpfiles.rules = [
+    "d /var/lib/microvm/searxng 0755 root root -"
+    "L+ /var/lib/microvm/searxng/secret_key - - - - ${config.sops.secrets.searxng_secret_key.path}"
+  ];
 }
