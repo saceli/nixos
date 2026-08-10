2f1c12ac46a360b72a9e6cf10c653372d35e0a2c
saceli <301811192+saceli@users.noreply.github.com>
2026-08-10 19:32:29 +0200

    sdfjnrehjejnverj

--- modules/cfg/flake.nix
+++ modules/cfg/flake.nix
@@ -4,7 +4,7 @@
   options.cfg.flake = {
 
     absolutePath = lib.mkOption {
-      type = lib.types.addCheck lib.types.path (p: builtins.pathExists p);
+      type = lib.types.path;
       default = /. + "${config.users.users.elia.home}/nixos"; # TODO: make the elia user a cfg option
       description = "Local flake path, the path you use to `nixos-rebuild THISPATH#outputName`";
     };
