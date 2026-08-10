f5b4a6ad5fa06a6fde7865295ea446dc26a43416
saceli <301811192+saceli@users.noreply.github.com>
2026-08-10 19:26:32 +0200

    sdfjnrehjejnverj

--- modules/cfg/homelab.nix
+++ modules/cfg/homelab.nix
@@ -25,8 +25,8 @@
     };
 
     authorizedKeysFile = lib.mkOption {
-      type = lib.types.path;
-      default = "${config.cfg.flake.absolutePath}/srv/microvm-authorized_keys";
+      type = lib.types.addCheck lib.types.path (p: builtins.pathExists p);
+      default = config.cfg.flake.absolutePath + /srv/microvm-authorized_keys;
       description = "SSH authorized keys file for MicroVMs";
     };
   };
