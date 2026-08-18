5057227342896fa1cec4191f78c9eb01e13e66c8
saceli <301811192+saceli@users.noreply.github.com>
2026-08-18 22:37:38 +0200

    good

--- modules/apps/gnome-keyring/enable.nix
+++ modules/apps/gnome-keyring/enable.nix
@@ -4,7 +4,5 @@
   services.gnome.gnome-keyring.enable = true;
   services.gnome.gcr-ssh-agent.enable = true;
 
-  programs.bash.initExtra = ''
-    export SSH_AUTH_SOCK="$XDG_RUNTIME_DIR/gcr/ssh"
-  '';
+  environment.variables.SSH_AUTH_SOCK = "$XDG_RUNTIME_DIR/gcr/ssh";
 }
